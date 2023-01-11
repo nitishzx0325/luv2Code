20d08560ab802ffb51545cec350fb172060b0caf
nitishzx0325 <nitishzx0325@gmail.com>
2023-01-11 09:51:47 +0530

    error

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,29 @@
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../../services/product.service';
+import { Product } from './../../common/product';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
+  constructor(
+    private productservice: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => this.handleProductDetails());
+  }
+  handleProductDetails(): void {
+    // throw new Error('Method not implemented.');
+    // get the "id" param String. convert String to a number by + sign
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+    this.productservice.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
+  }
+}
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,78 @@
+import { SearchComponent } from './../search/search.component';
+import { Product } from './../../common/product';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-grid.component.html',
+  // templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElement: number = 0;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => this.listProducts());
+  }
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProduct();
+    }
+  }
+  handleSearchProducts() {
+    // throw new Error('Method not implemented.');
+    const theKeyWord: string = this.route.snapshot.paramMap.get('keyword')!;
+    this.productService.SearchProducts(theKeyWord).subscribe((data) => {
+      this.products = data;
+    });
+  }
+  handleListProduct() {
+    //check if "Id" parameter isa vaialable
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      // get the id Param Strin. convertSring to a anumber by "+"
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // default category to be set 1
+      this.currentCategoryId = 1;
+    }
+    // check current pagination with product categoies
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(
+      `currentCategoryId=${this.currentCategoryId}.thePageNumer=${this.thePageNumber}`
+    );
+    this.productService
+      .getProductListPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        this.currentCategoryId
+      )
+      .subscribe((data) => {
+        this.products = data._embedded.products;
+        this.thePageNumber = data.page.number + 1;
+        this.thePageSize = data.page.size;
+        this.theTotalElement = data.page.totalElement;
+      });
+  }
+}
